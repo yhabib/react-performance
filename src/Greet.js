88cf780ba617e50ab86ef26f5193ae1b9ee5f35f
yhabib <habibfernandez@gmail.com>
2021-03-24 06:39:39 +0100

    Second example

---
+++ src/Greet.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+
+function Input({value, id, onChange}) {
+  return (
+    <label>
+      Name:
+      <input
+        id={id}
+        value={value}
+        onChange={e => onChange(e.target.value)}
+        type="text"
+      />
+    </label>
+  )
+}
+
+export default function Greet() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  return (
+    <div style={{marginTop: '50px'}}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <Input
+          id="name"
+          value={firstName}
+          onChange={setFirstName}
+        />
+      </div>
+      <div>
+        <label htmlFor="surname">Last name</label>
+        <Input
+          id="surname"
+          value={lastName}
+          onChange={setLastName}
+        />
+      </div>
+
+      <h2>My name is {`${lastName}, ${firstName} ${lastName}`}</h2>
+    </div>
+  )
+}
